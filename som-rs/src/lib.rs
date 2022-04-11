6e7032e61f84248b047705ab5d74f9547b90256c
Stefan Ulbrich <stefan.frank.ulbrich@gmail.com>
2022-04-12 00:49:06 +0200

    Fix accidental removal of lib.rs

--- som-rs/src/lib.rs
+++ som-rs/src/lib.rs
@@ -206,3 +206,28 @@ fn main() {
     nn = nn.adapt(&2.0);
     println!("{}", nn.neurons.lateral);
 }
+
+
+// #[cfg(test)]
+// mod tests {
+
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+
+//     // fn test_uniform() {
+//     //     let a = uniform((2, 3));
+//     //     let b = array![
+//     //         [0.0, 0.0],
+//     //         [0.0, 1.0],
+//     //         [1.0, 0.0],
+//     //         [1.0, 1.0],
+//     //         [2.0, 0.0],
+//     //         [2.0, 1.0]
+//     //     ];
+
+//     //     assert_eq!(a, b);
+//     // }
+// }
