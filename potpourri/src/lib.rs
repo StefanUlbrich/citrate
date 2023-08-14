6d9d246f52bff54f69bca8ab34755366fb635f02
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2023-08-14 18:08:09 +0200

    Minor fixes

--- potpourri/src/lib.rs
+++ potpourri/src/lib.rs
@@ -1,12 +1,12 @@
 use numpy::{IntoPyArray, PyReadonlyArray2, PyArray2, PyArray3, PyArray1};
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
-use ndarray::parallel::prelude::*;
 use ndarray::prelude::*;
 
 
 #[pymodule]
-fn potpourri(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+#[pyo3(name = "potpourri")]
+fn pypotpourri(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     // example using immutable borrows producing a new array
     // fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
     //     a * &x + &y
